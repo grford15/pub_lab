54adb35e6b5a1a0190f8544b4b2678756dea3507
Jack Murning <jackrichardmurning@gmail.com>
2018-05-23 11:29:44 +0100

    first pub commit, with environment setup

---
+++ specs/drinks_spec.rb
@@ -0,0 +1,8 @@
+require "minitest/autorun"
+require "minitest/rg"
+
+require_relative('../drinks.rb')
+
+class TestDrinks < MiniTest::Test
+
+end
---
+++ specs/pub_spec.rb
@@ -0,0 +1,7 @@
+require "minitest/autorun"
+require "minitest/rg"
+require_relative('../pub.rb')
+
+class TestPub
+
+end
